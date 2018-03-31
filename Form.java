fb6cb423b7d76e1a0ed73e480ff60aaabefc40b4
Kaedon <kaedon33@gmail.com>
2018-03-31 13:02:37 +0200

    UI fixes

--- Form.java
+++ Form.java
@@ -3,6 +3,21 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.AbstractAction;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.KeyStroke;
+
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
@@ -12,19 +27,71 @@ import java.awt.Dimension;
 public class Form
 	extends JFrame{
 
-	private static JPanel mainPanel, dataEntry, oldNames;
-	private static JLabel lblname1, lblname2, lblname3, lblname4, lblname5, lblname6, lblid1, lblid2, lblid3, lblid4, lblid5, lblid6;
-	private static JTextField txfEntry;
-	private static JButton btnCheckID;
+	private JPanel mainPanel, dataEntry, oldNames;
+	private JLabel lblname1, lblname2, lblname3, lblname4, lblname5, lblname6, lblid1, lblid2, lblid3, lblid4, lblid5, lblid6, lblstuff;
+	private JTextField txfEntry;
+	private JButton btnCheckID;
+	private AbstractAction enterOrButton, pasteAction;
 
 	public static void main(String[] args)
 	{
-		new Form();
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				new Form();
+			}
+		});
 	}
 
 	public Form()
 	{
 		mainPanel = new JPanel(new GridLayout(3,1));
+
+		enterOrButton = new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				String ID = txfEntry.getText();
+
+		                if (ID.length() == 13)
+        	        	{
+                	        	addNewID(ID + "  ", "Kaedon Jon Williams");
+					txfEntry.setText("Enter ID number here.");
+	                	}
+        	        System.out.println("Pressed!");
+			}
+		};
+
+		pasteAction = new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				try
+				{
+					String s = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+					String r = s.replaceAll("[^\\d]", "");
+					if (s.equals(r) || (JOptionPane.showConfirmDialog(null,"Your clipboard contains illegal characters. Would you like to paste just the numeric characters?", "Warning", JOptionPane.YES_NO_OPTION)) == JOptionPane.YES_OPTION);
+					{
+						if (!(txfEntry.getSelectedText() == null))
+						{
+							txfEntry.replaceSelection(r);
+						}else{
+							String oldText = txfEntry.getText();
+							int i = txfEntry.getCaretPosition();
+							String tmp = oldText.substring(0,i) + r + oldText.substring(i);;
+							txfEntry.setText(tmp);
+							txfEntry.setCaretPosition(i + r.length());
+						}
+
+						String oldText = txfEntry.getText();
+						oldText = oldText.replaceAll("[^\\d]", "");
+						txfEntry.setText(oldText);
+					}
+				}catch(Exception ex){
+					System.err.println(ex.getMessage());
+				}
+			}
+		};
 
 		mainPanel.add(dataEntry());
 		mainPanel.add(lastName());
@@ -38,7 +105,7 @@ public class Form
 		setVisible(true);
 	}
 
-	public static void addNewID(String ID, String name)
+	public void addNewID(String ID, String name)
 	{
 		lblid6.setText(lblid5.getText());
 		lblid5.setText(lblid4.getText());
@@ -55,12 +122,12 @@ public class Form
 		lblname1.setText(name);
 	}
 
-	public static void addNewID(String ID)
+	public void addNewID(String ID)
 	{
 		addNewID(ID, "ID not found.");
 	}
 
-	private static JPanel lastName()
+	private JPanel lastName()
 	{
 		Font vd = new Font("Verdana", 2, 40);
 		JPanel pn = new JPanel(new GridLayout(2,2));
@@ -68,8 +135,8 @@ public class Form
 		JLabel ID = new JLabel("ID:");
 		JLabel Name = new JLabel("Name:");
 
-		lblid1 = new JLabel("id");
-		lblname1 = new JLabel("name");
+		lblid1 = new JLabel("");
+		lblname1 = new JLabel("");
 
 		ID.setHorizontalAlignment(JLabel.CENTER);
 		Name.setHorizontalAlignment(JLabel.CENTER);
@@ -89,22 +156,22 @@ public class Form
 		return pn;
 	}
 
-	private static JPanel oldNames()
+	private JPanel oldNames()
 	{
 		JPanel pn = new JPanel(new GridLayout(5,2));
 		Font vd = new Font("Verdana", 2, 30);
 
-		lblid2 = new JLabel("id1");
-		lblid3 = new JLabel("id2");
-		lblid4 = new JLabel("id3");
-		lblid5 = new JLabel("id4");
-		lblid6 = new JLabel("id5");
+		lblid2 = new JLabel("");
+		lblid3 = new JLabel("");
+		lblid4 = new JLabel("");
+		lblid5 = new JLabel("");
+		lblid6 = new JLabel("");
 
-		lblname2 = new JLabel("1");
-		lblname3 = new JLabel("2");
-		lblname4 = new JLabel("3");
-		lblname5 = new JLabel("4");
-		lblname6= new JLabel("5");
+		lblname2 = new JLabel("");
+		lblname3 = new JLabel("");
+		lblname4 = new JLabel("");
+		lblname5 = new JLabel("");
+		lblname6= new JLabel("");
 
 		lblname2.setHorizontalAlignment(JLabel.LEFT);
 		lblname3.setHorizontalAlignment(JLabel.LEFT);
@@ -148,7 +215,7 @@ public class Form
 		return pn;
 	}
 
-	private static JPanel dataEntry()
+	private JPanel dataEntry()
 	{
 		JPanel pnl = new JPanel(new FlowLayout(FlowLayout.CENTER, 30, 30));
 		Font vd = new Font("Verdana", 2, 30);
@@ -156,13 +223,30 @@ public class Form
 		txfEntry = new JTextField("Enter ID number here.", 20);
 		txfEntry.setPreferredSize(new Dimension(400, 60));
 		txfEntry.setFont(vd);
+		txfEntry.addKeyListener(new KeyAdapter(){
+			public void keyTyped(KeyEvent e){
+				char typed = e.getKeyChar();
+				if ((typed < '0' || typed > '9'))
+					e.consume();
+			}
+		});
+		txfEntry.addActionListener(enterOrButton);
+		txfEntry.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK), "pressed");
+		txfEntry.getActionMap().put("pressed", pasteAction);
 
 		btnCheckID = new JButton("Check ID");
 		btnCheckID.setPreferredSize(new Dimension(200, 60));
+		btnCheckID.addActionListener(enterOrButton);
+
+		lblstuff = new JLabel();
+
 		txfEntry.setFont(vd);
+		btnCheckID.setFont(vd);
+		lblstuff.setFont(vd);
 
 		pnl.add(txfEntry);
 		pnl.add(btnCheckID);
+		pnl.add(lblstuff);
 
 		return pnl;
 	}
